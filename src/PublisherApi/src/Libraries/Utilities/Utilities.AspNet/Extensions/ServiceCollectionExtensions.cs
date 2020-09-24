89e42770c9abec05a6c4ea37a8c1aa6b1676915c
Mike Sadler <msadler@gnolaum.com>
2020-09-23 20:13:47 -0500

    Initial

---
+++ src/PublisherApi/src/Libraries/Utilities/Utilities.AspNet/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System;
+using Utilities.Settings;
+
+namespace Utilities.AspNet.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void ConfigureRedis(this IServiceCollection services, Action<Redis> setup)
+        {
+            if (services == null) { throw new ArgumentNullException(nameof(services)); }
+
+            var redisOptions = new Redis();
+
+            setup(redisOptions);
+
+            var connectionString = redisOptions.ConnectionString;
+
+            var redis = ConnectionMultiplexer.Connect(connectionString);
+
+            services.AddDataProtection()
+                    .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+        }
+    }
+}
---
+++ src/Worker/src/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Newtonsoft.Json;
+
+namespace TwitchBot
+{
+    class Program
+    {
+        #region Configuration
+        public static Action<IConfigurationBuilder> BuildConfiguration =
+            builder => builder.SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                              .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                              .AddEnvironmentVariables();
+        #endregion
+
+        public static async Task PostMessage(string postData)
+        {
+            var json = JsonConvert.SerializeObject(postData);
+            var content = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            using (var httpClientHandler = new HttpClientHandler())
+            {
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                using (var client = new HttpClient(httpClientHandler))
+                {
+                    var result = await client.PostAsync("https://192.168.7.10:5001/api/values", content);
+                    string resultContent = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine("Server returned: " + resultContent);
+                }
+            }
+        }
+        
+        public static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfiguration(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                            .ReadFrom.Configuration(builder.Build())
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console()
+                            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+
+                PostMessage("test message").Wait();
+
+                var host = CreateHostBuilder(args).Build();
+
+                await host.RunAsync();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: false, reloadOnChange: true);
+                    configHost.AddEnvironmentVariables(prefix: "WORKER_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    configApp.AddEnvironmentVariables();
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+
+                })
+                .UseSerilog();
+    }
+}
