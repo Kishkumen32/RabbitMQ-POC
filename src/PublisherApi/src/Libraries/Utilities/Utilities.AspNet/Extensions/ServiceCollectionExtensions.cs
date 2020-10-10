c002b42cdac6d65097900e8a3ee55ac672883269
Mike Sadler <msadler@gnolaum.com>
2020-10-09 21:13:20 -0500

    doing some docker-compose stuff

--- src/PublisherApi/src/Libraries/Utilities/Utilities.AspNet/Extensions/ServiceCollectionExtensions.cs
+++ src/PublisherApi/src/Libraries/Utilities/Utilities.AspNet/Extensions/ServiceCollectionExtensions.cs
@@ -16,6 +16,8 @@ namespace Utilities.AspNet.Extensions
 
             setup(redisOptions);
 
+            var logger = services.
+
             var connectionString = redisOptions.ConnectionString;
 
             var redis = ConnectionMultiplexer.Connect(connectionString);
